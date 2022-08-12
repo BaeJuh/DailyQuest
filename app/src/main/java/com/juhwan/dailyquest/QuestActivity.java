ea82b21e2eaf34da9718dfab9bead5d0e23c4d9a
BaeJuh <ghkslqlsl04@gmail.com>
2022-08-12 17:12:18 +0900

    update_dialog

--- app/src/main/java/com/juhwan/dailyquest/QuestActivity.java
+++ app/src/main/java/com/juhwan/dailyquest/QuestActivity.java
@@ -1,14 +1,17 @@
 package com.juhwan.dailyquest;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -20,9 +23,16 @@ public class QuestActivity extends AppCompatActivity {
     private Button addButton;
     private ListView container;
 
-    ScheduleAdapter adapter = new ScheduleAdapter();
+    private LinearLayout dialog;
+    private EditText Edit_schedule;
+    private EditText Edit_hour;
+    private EditText Edit_minute;
 
-    int i=10;
+    String s = "";
+    int h = 0;
+    int m = 0;
+
+    ScheduleAdapter adapter = new ScheduleAdapter();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,15 +48,40 @@ public class QuestActivity extends AppCompatActivity {
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                addSchedule();
-
+                show_dialog();
             }
         });
     }
 
-    public void addSchedule() {
-        adapter.addItem("제목", i, i);
+    public void addSchedule(String s ,int h, int m) {
+        adapter.addItem(s, h, m);
         adapter.notifyDataSetChanged();
+    }
+
+    public void show_dialog() {
+        dialog= (LinearLayout) View.inflate(QuestActivity.this, R.layout.dialog, null);
+        Edit_schedule=(EditText) dialog.findViewById(R.id.Edit_schedule);
+        Edit_hour=(EditText) dialog.findViewById(R.id.Edit_hour);
+        Edit_minute=(EditText) dialog.findViewById(R.id.Edit_minute);
+
+
+
+        AlertDialog.Builder dig = new AlertDialog.Builder(QuestActivity.this);
+        dig.setTitle("시간 입력");
+        dig.setView(dialog);
+        dig.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                s = Edit_schedule.getText().toString();
+                h = Integer.parseInt(String.valueOf(Edit_hour.getText()));
+                m = Integer.parseInt(String.valueOf(Edit_minute.getText()));
+
+                addSchedule(s, h, m);
+            }
+        });
+
+        dig.setNegativeButton("취소", null);
+        dig.show();
     }
 
 }
